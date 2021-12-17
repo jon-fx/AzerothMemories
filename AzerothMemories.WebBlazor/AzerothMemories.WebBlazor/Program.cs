9d23207213802c0a09d76ea21d8d21bf2102552e
John <johnbourne88@live.co.uk>
2021-12-17 18:02:15 +0000

    Update project files

--- AzerothMemories.WebBlazor/AzerothMemories.WebBlazor/Program.cs
+++ AzerothMemories.WebBlazor/AzerothMemories.WebBlazor/Program.cs
@@ -1,10 +1,8 @@
-using AzerothMemories.WebBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+//builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
--- AzerothMemories.WebServer/AzerothMemories.WebServer/Program.cs
+++ AzerothMemories.WebServer/AzerothMemories.WebServer/Program.cs
@@ -1,39 +1,35 @@
+using MudBlazor.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddMudServices();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseWebAssemblyDebugging();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 else
 {
-    app.UseExceptionHandler("/Error");
+    app.UseStatusCodePages();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
-
-//app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+    endpoints.MapFallbackToPage("/_Host");
+});
 
 app.Run();
