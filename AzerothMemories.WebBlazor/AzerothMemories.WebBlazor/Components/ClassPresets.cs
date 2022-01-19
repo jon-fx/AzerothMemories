b289c96db0e2418de6376f3126011d6f87b58d8a
John <johnbourne88@live.co.uk>
2022-01-19 22:25:54 +0000

    Add ClassPresents to provide consistent margins

---
+++ AzerothMemories.WebBlazor/AzerothMemories.WebBlazor/Components/ClassPresets.cs
@@ -0,0 +1,12 @@
+﻿namespace AzerothMemories.WebBlazor.Components
+{
+    public static class ClassPresets
+    {
+        public const string Card = "pa-2 ma-2 card";
+        public const string CardHeader = "pa-2 pa-2";
+        public const string CardActions = "pa-2 pa-2";
+        public const string CardContent = "pa-2 pa-2";
+
+        public const string CardHeaderPostCard = "pa-2 ma-2 card postCard";
+    }
+}
