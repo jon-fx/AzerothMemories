2d63041629bfa60ad21106466c2a47004b684c20
John <johnbourne88@live.co.uk>
2022-01-28 22:08:41 +0000

    WIP

--- AzerothMemories.WebBlazor/AzerothMemories.WebBlazor/Services/Commands/Character_TryEnqueueUpdate.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AzerothMemories.WebBlazor.Services.Commands;
-
-public record Character_TryEnqueueUpdate(Session Session) : ICommand<bool>
-{
-    public Character_TryEnqueueUpdate() : this(Session.Null)
-    {
-    }
-}
