38d74e307a9c6efdc8b23e2fff52551fca50109c
John <johnbourne88@live.co.uk>
2022-02-04 00:37:47 +0000

    WIP

--- AzerothMemories.WebServer/AzerothMemories.WebServer/Program.cs
+++ AzerothMemories.WebServer/AzerothMemories.WebServer/Program.cs
@@ -7,7 +7,12 @@ using Stl.Fusion.Server.Controllers;
 using System.Net.Http.Headers;
 using System.Text;
 
-var config = new CommonConfig(false);
+var azure = true;
+#if DEBUG
+azure = false;
+#endif
+
+var config = new CommonConfig(azure);
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddLogging(logging =>
