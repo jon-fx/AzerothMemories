5a281f36ce13546bb563c8f579cfc9fd3a615235
John <johnbourne88@live.co.uk>
2022-02-04 00:14:01 +0000

    WIP

---
+++ AzerothMemories.Clean.bat
@@ -0,0 +1,17 @@
+del /f/s/q AzerothMemories.Common\AzerothMemories.Common\obj > nul
+rmdir /s/q AzerothMemories.Common\AzerothMemories.Common\obj
+
+del /f/s/q AzerothMemories.Database\AzerothMemories.Database\obj > nul
+rmdir /s/q AzerothMemories.Database\AzerothMemories.Database\obj
+
+del /f/s/q AzerothMemories.Database.Seeder\AzerothMemories.Database.Seeder\obj > nul
+rmdir /s/q AzerothMemories.Database.Seeder\AzerothMemories.Database.Seeder\obj
+
+del /f/s/q AzerothMemories.WebBlazor\AzerothMemories.WebBlazor\obj > nul
+rmdir /s/q AzerothMemories.WebBlazor\AzerothMemories.WebBlazor\obj
+
+del /f/s/q AzerothMemories.WebServer\AzerothMemories.WebServer\obj > nul
+rmdir /s/q AzerothMemories.WebServer\AzerothMemories.WebServer\obj
+
+del /f/s/q AzerothMemories.Build > nul
+rmdir /s/q AzerothMemories.Build
--- AzerothMemories.WebServer/AzerothMemories.WebServer/Program.cs
+++ AzerothMemories.WebServer/AzerothMemories.WebServer/Program.cs
@@ -7,7 +7,7 @@ using Stl.Fusion.Server.Controllers;
 using System.Net.Http.Headers;
 using System.Text;
 
-var config = new CommonConfig(true);
+var config = new CommonConfig(false);
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddLogging(logging =>
