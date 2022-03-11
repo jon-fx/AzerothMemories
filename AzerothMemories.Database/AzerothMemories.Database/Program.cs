803d7cb591f7362dbff91ac72fb82f20478aaf26
John <johnbourne88@live.co.uk>
2022-03-11 14:27:42 +0000

    WIP

--- AzerothMemories.Database/AzerothMemories.Database/Program.cs
+++ AzerothMemories.Database/AzerothMemories.Database/Program.cs
@@ -13,26 +13,35 @@ services.AddFluentMigratorCore()
         .WithGlobalConnectionString(config.DatabaseConnectionString)
         .ScanIn(typeof(Migration0001_EntiyFramework).Assembly).For.Migrations());
 
+if (config.DatabaseConnectionString.Contains("azure"))
+{
+    if (ConfigHelpers.SaftyCheck("DO YOU REALLY WANT TO MODIFY AN AZURE DATABASE?!"))
+    {
+    }
+    else
+    {
+        throw new NotImplementedException();
+    }
+}
+
 var serviceProvider = services.BuildServiceProvider(true);
 
 using var scope = serviceProvider.CreateScope();
 var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
 
-if (ConfigHelpers.SaftyCheck("DELETE ALL ACCOUNT DATA?!"))
-{
-    runner.MigrateDown(Migration0003_AccountData.MigrationId - 1);
-}
-
-if (ConfigHelpers.SaftyCheck("DELETE ALL BLIZZARD DATA?!"))
-{
-    runner.MigrateDown(Migration0002_BlizzardData.MigrationId - 1);
-}
-
-if (ConfigHelpers.SaftyCheck("DELETE ENTITY FRAMEWORK DATA?!"))
+if (ConfigHelpers.SaftyCheck("DELETE ABSOLUTELY EVERYTHING?!"))
 {
     runner.MigrateDown(Migration0001_EntiyFramework.MigrationId - 1);
 
     runner.Processor.Execute("DELETE FROM \"VersionInfo\"");
+}
+else if (ConfigHelpers.SaftyCheck("DELETE BLIZZARD DATA AND ACCOUNTS?!"))
+{
+    runner.MigrateDown(Migration0002_BlizzardData.MigrationId - 1);
+}
+else if (ConfigHelpers.SaftyCheck("DELETE ACCOUNT DATA?!"))
+{
+    runner.MigrateDown(Migration0003_AccountData.MigrationId - 1);
 }
 
 ConfigHelpers.WriteWithColors(ConsoleColor.Green, ConsoleColor.White, "Running Migration...");
