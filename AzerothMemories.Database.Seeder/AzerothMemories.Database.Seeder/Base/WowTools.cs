27cfc3f1827e50168b7b8055d364fa081ee85050
John <johnbourne88@live.co.uk>
2022-03-15 17:07:13 +0000

    WIP

--- AzerothMemories.Database.Seeder/AzerothMemories.Database.Seeder/Base/WowTools.cs
+++ AzerothMemories.Database.Seeder/AzerothMemories.Database.Seeder/Base/WowTools.cs
@@ -2,7 +2,7 @@
 
 internal sealed class WowTools
 {
-    private const string BuildConst = "9.2.0.42614";
+    private const string BuildConst = "9.2.0.42698";
 
     public WowToolsInternal Main { get; } = new(BuildConst, true);
 }
