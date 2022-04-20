f5bc35c206d96c52f3f2d827314c43178f2cc77d
John <johnbourne88@live.co.uk>
2022-04-20 20:06:54 +0100

    Update Wow version & PostTagChips

--- AzerothMemories.Database.Seeder/AzerothMemories.Database.Seeder/Base/WowTools.cs
+++ AzerothMemories.Database.Seeder/AzerothMemories.Database.Seeder/Base/WowTools.cs
@@ -2,7 +2,7 @@
 
 internal sealed class WowTools
 {
-    private const string BuildConst = "9.2.0.43114";
+    private const string BuildConst = "9.2.0.43206";
 
     public WowToolsInternal Main { get; } = new(BuildConst, true);
 }
