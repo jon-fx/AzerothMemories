8914bfbba93fe6b8526a1b80125a19b6c1ecf78b
John <johnbourne88@live.co.uk>
2022-04-27 15:04:38 +0100

    Update Wow version

--- AzerothMemories.Database.Seeder/AzerothMemories.Database.Seeder/Base/WowTools.cs
+++ AzerothMemories.Database.Seeder/AzerothMemories.Database.Seeder/Base/WowTools.cs
@@ -2,7 +2,7 @@
 
 internal sealed class WowTools
 {
-    private const string BuildConst = "9.2.0.43206";
+    private const string BuildConst = "9.2.0.43340";
 
     public WowToolsInternal Main { get; } = new(BuildConst, true);
 }
