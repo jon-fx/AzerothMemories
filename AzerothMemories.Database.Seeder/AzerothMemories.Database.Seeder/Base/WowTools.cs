b132c4ecfdf37aff559683070db7726ac05b2349
John <johnbourne88@live.co.uk>
2022-09-24 18:06:00 +0100

    Update dependencies

--- AzerothMemories.Database.Seeder/AzerothMemories.Database.Seeder/Base/WowTools.cs
+++ AzerothMemories.Database.Seeder/AzerothMemories.Database.Seeder/Base/WowTools.cs
@@ -2,7 +2,7 @@
 
 internal sealed class WowTools
 {
-    private const string BuildConst = "9.2.7.45338";
+    private const string BuildConst = "9.2.7.45745";
 
     public WowToolsInternal Main { get; } = new(BuildConst, true);
 }
