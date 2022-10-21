741dac61c08f0f38eac1c5198caf4eb4ff4594de
John <johnbourne88@live.co.uk>
2022-10-21 13:08:08 +0100

    Throw error when resource counts don'\''t match

--- AzerothMemories.Database.Seeder/AzerothMemories.Database.Seeder/Base/MoaResourceWriter.cs
+++ AzerothMemories.Database.Seeder/AzerothMemories.Database.Seeder/Base/MoaResourceWriter.cs
@@ -304,6 +304,11 @@ internal sealed class MoaResourceWriter
             currentData.Name.ZhCn = serverSideResource.GetNameOrDefault(ServerSideLocale.Zh_Cn);
         }
 
+        if (database.BlizzardData.Count() != _serverSideResources.Count)
+        {
+            throw new NotImplementedException();
+        }
+
         await database.SaveChangesAsync();
 
         _logger.LogInformation("End Save");
