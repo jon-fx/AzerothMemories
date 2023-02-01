6fc67ac40db624241e0f1a6011860d9e8042d61d
John <johnbourne88@live.co.uk>
2023-02-01 16:11:51 +0000

    Update dependencies

--- AzerothMemories.Database.Seeder/AzerothMemories.Database.Seeder/Base/WowTools.cs
+++ AzerothMemories.Database.Seeder/AzerothMemories.Database.Seeder/Base/WowTools.cs
@@ -1,8 +1,8 @@
-﻿namespace AzerothMemories.Database.Seeder.Base;
+﻿using AzerothMemories.WebServer.Common;
+
+namespace AzerothMemories.Database.Seeder.Base;
 
 internal sealed class WowTools
 {
-    private const string BuildConst = "10.0.5.47849";
-
-    public WowToolsInternal Main { get; } = new(BuildConst, true);
+    public WowToolsInternal Main { get; } = new(CommonConfigDoNotCommit.CurrentBuildConst, true);
 }
--- AzerothMemories.WebBlazor/AzerothMemories.WebBlazor/ProgramEx.cs
+++ AzerothMemories.WebBlazor/AzerothMemories.WebBlazor/ProgramEx.cs
@@ -1,4 +1,11 @@
-﻿namespace AzerothMemories.WebBlazor;
+﻿using Stl.Fusion.Bridge.Interception;
+using Stl.Fusion.Diagnostics;
+using Stl.Fusion.Extensions;
+using Stl.Fusion.Internal;
+using Stl.OS;
+using Stl.Time;
+
+namespace AzerothMemories.WebBlazor;
 
 public static class ProgramEx
 {
@@ -28,5 +35,44 @@ public static class ProgramEx
         services.AddScoped<ClientServices>();
         services.AddScoped<ActiveAccountServices>();
         services.AddScoped<DialogHelperService>();
+
+        // Other UI-related services
+        var fusion = services.AddFusion();
+        fusion.AddComputedGraphPruner(_ => new ComputedGraphPruner.Options { CheckPeriod = TimeSpan.FromSeconds(30) });
+        fusion.AddFusionTime();
+        fusion.AddBackendStatus();
+
+        // Default update delay is 0.5s
+        services.AddScoped<IUpdateDelayer>(c => new UpdateDelayer(c.UIActionTracker(), 0.5));
+
+        // Diagnostics
+        services.AddHostedService(c =>
+        {
+            var isWasm = OSInfo.IsWebAssembly;
+            return new FusionMonitor(c)
+            {
+                SleepPeriod = isWasm ? TimeSpan.Zero : TimeSpan.FromMinutes(1).ToRandom(0.25),
+                CollectPeriod = TimeSpan.FromSeconds(isWasm ? 3 : 60),
+                AccessFilter = isWasm ? static computed => computed.Input.Function is IReplicaMethodFunction : static _ => true,
+                AccessStatisticsPreprocessor = StatisticsPreprocessor,
+                RegistrationStatisticsPreprocessor = StatisticsPreprocessor,
+            };
+
+            static void StatisticsPreprocessor(Dictionary<string, (int, int)> stats)
+            {
+                foreach (var key in stats.Keys.ToList())
+                {
+                    if (key.Contains(".Pseudo"))
+                    {
+                        stats.Remove(key);
+                    }
+
+                    if (key.StartsWith("FusionTime."))
+                    {
+                        stats.Remove(key);
+                    }
+                }
+            }
+        });
     }
 }
