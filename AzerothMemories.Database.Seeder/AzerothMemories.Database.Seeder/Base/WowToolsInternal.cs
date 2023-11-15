6941772cefa52b97c57a2a44e6f87fa86bfcda63
Jon <johnbourne88@live.co.uk>
2023-11-15 20:25:24 +0000

    update 10.2.0.52148

--- AzerothMemories.Database.Seeder/AzerothMemories.Database.Seeder/Base/WowToolsInternal.cs
+++ AzerothMemories.Database.Seeder/AzerothMemories.Database.Seeder/Base/WowToolsInternal.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using AzerothMemories.WebServer.Common;
+using System.Net;
 
 namespace AzerothMemories.Database.Seeder.Base;
 
@@ -122,7 +123,7 @@ internal sealed class WowToolsInternal
 
     public void LoadDataFromWowTools(string fileName, string primaryKeyName, ref Dictionary<int, WowToolsData> dictionary, string locale, string[] fieldsToLoad = null)
     {
-        var fileInfo = DownloadIfNotExists($"{fileName}-{locale}.csv", $"http://localhost:5080/dbc/export/?name={fileName}&build={_buildString}&locale={locale}");
+        var fileInfo = DownloadIfNotExists($"{fileName}-{locale}.csv", $"{CommonConfigDoNotCommit.LocalWowToolsUrl}/dbc/export/?name={fileName}&build={_buildString}&locale={locale}");
         if (fileInfo == null)
         {
             return;
