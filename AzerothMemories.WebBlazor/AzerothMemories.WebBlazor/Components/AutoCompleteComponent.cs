d4e8e3a6956fa6084820e38558423340d940c974
Jon <johnbourne88@live.co.uk>
2024-03-06 19:08:18 +0000

    fix: auto complete

--- AzerothMemories.WebBlazor/AzerothMemories.WebBlazor/Components/AutoCompleteComponent.cs
+++ AzerothMemories.WebBlazor/AzerothMemories.WebBlazor/Components/AutoCompleteComponent.cs
@@ -14,5 +14,6 @@ public sealed class AutoCompleteComponent<T> : MudAutocomplete<T>
         MaxItems = null;
         InputMode = InputMode.search;
         DebounceInterval = 500;
+        SelectOnClick = false;
     }
 }
