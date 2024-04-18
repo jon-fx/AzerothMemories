f1ad9ae3b82616c36afe6211248d96d995051212
Jon <johnbourne88@live.co.uk>
2024-04-18 21:51:26 +0100

    fix: misc

--- AzerothMemories.WebBlazor/AzerothMemories.WebBlazor/Pages/AccountHistoryPageViewModel.cs
+++ AzerothMemories.WebBlazor/AzerothMemories.WebBlazor/Pages/AccountHistoryPageViewModel.cs
@@ -34,7 +34,7 @@ public sealed class AccountHistoryPageViewModel : ViewModelBase, IViewModel<Acco
             }
             else
             {
-                currentPage = Math.Clamp(currentPage, 0, TotalPages);
+                currentPage = Math.Clamp(currentPage, 1, TotalPages);
             }
         }
 
